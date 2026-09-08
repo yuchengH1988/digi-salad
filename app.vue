f03e9dd437352f7b60f543c535b66cc7c2d3c2d0
CalvinHuang <yucheng.h.1988@gmail.com>
2026-09-08 21:21:21 +0800

    ffix hero rwd

--- app.vue
+++ app.vue
@@ -1,5 +1,54 @@
 <script setup>
 import 'virtual:svg-icons-register'
+
+const isMobileDevice = () => {
+  if (typeof navigator === 'undefined') return false
+
+  const ua = navigator.userAgent || navigator.vendor || ''
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(String(ua))
+}
+
+let isMobile = false
+
+const updateWindowVh = () => {
+  if (!process.client) return
+
+  document.documentElement.style.setProperty(
+    '--windowVh',
+    isMobile ? `${window.innerHeight}px` : '100vh'
+  )
+}
+
+const handleResize = () => {
+  if (!isMobile) {
+    updateWindowVh()
+  }
+}
+
+const handleOrientationChange = () => {
+  updateWindowVh()
+}
+
+onMounted(() => {
+  isMobile = isMobileDevice()
+  updateWindowVh()
+
+  if (isMobile) {
+    setTimeout(() => {
+      if (window.scrollY === 0) {
+        updateWindowVh()
+      }
+    }, 500)
+  }
+
+  window.addEventListener('resize', handleResize)
+  window.addEventListener('orientationchange', handleOrientationChange)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', handleResize)
+  window.removeEventListener('orientationchange', handleOrientationChange)
+})
 </script>
 
 <template>
--- components/Section/Hero.vue
+++ components/Section/Hero.vue
@@ -5,32 +5,35 @@ import salad from '~/assets/images/salad.png'
 </script>
 
 <template>
-  <section id="top" class="relative h-[1025px] bg-page text-white max-md:h-[760px]">
-    <div class="relative h-[940px] overflow-hidden rounded-b-[50px] max-md:h-[690px] max-md:rounded-b-[30px]">
+  <section id="top" class="relative h-[var(--windowVh,100vh)] text-white">
+    <div class="absolute inset-0 overflow-hidden rounded-b-[30px] lg:rounded-b-[50px]">
       <BasePicture :src="heroDesktop" :mobile-src="heroMobile" alt="Hong Kong city light trails at night" loading="eager" img-class="absolute inset-0 size-full object-cover" />
       <div class="absolute inset-0 bg-[rgba(12,38,34,0.28)]"></div>
-
-      <div class="absolute left-[15.28%] top-[181px] w-[199px] max-md:left-6 max-md:top-[120px] max-md:w-[145px]">
+    </div>
+    <div class="relative h-[var(--windowVh,100vh)]">
+      <div class="absolute left-15 md:left-24 top-[17.39%] w-[145px] lg:left-[15.28%] lg:top-[19.26%] lg:w-[199px]">
         <AtomIcon name="icon" is-full class="text-white" />
       </div>
-      <div class="absolute left-[15.69%] top-[303px] max-md:left-6 max-md:right-6 max-md:top-[235px]" data-fade="up">
-        <h1 class="text-display-1">
+      <div class="absolute left-15 md:left-24 top-[34.06%] lg:left-[15.69%] lg:top-[32.23%]" data-fade="up">
+        <h1 class="text-display-1 relative">
           We create<br>
           amazing<br>
-          digital experiences<span class="text-coral">.</span>
+          digital <span class="block sm:inline-block">experiences</span>
+          <div class="absolute -right-4 bottom-4 size-[10px] bg-coral rounded-full"></div>
         </h1>
-        <div class="mt-[-24px] h-1 w-[571px] max-w-full bg-accent"></div>
+        <div class="mt-[-18px] lg:mt-[-24px] h-1 w-[250px] sm:w-[410px] lg:w-[571px] max-w-full bg-accent"></div>
       </div>
-      <div class="absolute left-[50px] top-[177px] flex h-[386px] w-6 flex-col items-center max-md:hidden">
+      <div class="absolute left-[3.6%] top-[35.5%] h-[386px] w-6 flex-col items-center flex">
         <span class="text-mini-1 origin-center -rotate-90 whitespace-nowrap">Digital agency</span>
         <span class="mt-[109px] h-[113px] w-[2px] bg-white"></span>
       </div>
     </div>
 
-    <div class="absolute left-1/2 top-[772px] flex -translate-x-1/2 flex-col items-center max-md:top-[640px]">
+    <div class="absolute left-1/2 flex -translate-x-1/2 flex-col items-center bottom-[-86px]">
       <img :src="salad" alt="" class="size-[65px]">
       <span class="text-mini-1 mt-3 whitespace-nowrap">Taste us now!</span>
-      <span class="mt-3 h-[130px] w-px bg-ink"></span>
+      <span class="mt-3 h-[44px] w-px bg-white"></span>
+      <span class="mt-0 h-[86px] w-px bg-ink"></span>
     </div>
   </section>
 </template>
